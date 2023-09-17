635324029766ebd4aaf2a6595f50cf9be634b441
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-09-16 18:04:32 -0700

    task 4.edit2

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- a script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 GRANT SHOW DATABASES ON *.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
