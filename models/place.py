c5966282b5a949cecd43b134f2f2fe0104ed4103
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-09-20 15:19:35 -0700

    edit 10.places

--- models/place.py
+++ models/place.py
@@ -38,7 +38,8 @@ class Place(BaseModel, Base):
         reviews = relationship(
             "Review", cascade="all, delete", backref="place")
         amenities = relationship(
-                "Amenity", secondary="place_amenity", viewonly=False, backref="place_amenities")
+            "Amenity", secondary="place_amenity", viewonly=False,
+            backref="place_amenities")
     else:
         @property
         def reviews(self):
