1c187dc9a9f85f1b5b155b0e19ec05042530ed11
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-09-19 18:06:20 -0700

    PyStyle

--- models/review.py
+++ models/review.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey, Column, String
 
+
 class Review(BaseModel, Base):
     """Review classto store review information"""
     __tablename__ = "reviews"
