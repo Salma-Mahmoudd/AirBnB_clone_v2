2045c23455dcd627c5bd0fc74202fabf6771759b
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-10-22 15:59:19 -0700

    Improve engines

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """State Module for HBNB project"""
 import models
+from models.city import City
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
