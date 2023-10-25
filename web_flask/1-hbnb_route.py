d0079f40cbf7b979ff07e0cf09c30516ae5adff9
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-10-25 04:53:46 -0700

    Recover the deleted directory

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def welcome_page():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run()
